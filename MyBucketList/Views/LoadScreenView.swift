50996b8bb5a595fe694905aaa7a5ecba0ca94e60
Dominic Dobbrunz <d.dobbrunz@gmx.de>
2025-02-11 13:54:49 +0100

    LoadScreen hinzugefügt

---
+++ MyBucketList/Views/LoadScreenView.swift
@@ -0,0 +1,23 @@
+//
+//  LoadScreenView.swift
+//  MyBucketList
+//
+//  Created by Dominic Dobbrunz on 11.02.25.
+//
+
+
+import SwiftUI
+
+struct LoadScreenView: View {
+    @EnvironmentObject private var userViewModel: UserViewModel
+    var body: some View {
+        LoadView(title: "My Bucket List", image: .background1, content: LoginView())
+        
+    }
+}
+
+#Preview {
+    @Previewable @StateObject var userVM: UserViewModel = .init()
+    LoadScreenView()
+        .environmentObject(userVM)
+}
